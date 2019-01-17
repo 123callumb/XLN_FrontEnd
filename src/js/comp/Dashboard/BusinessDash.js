223eec1ff5e2cd0ef977879a03bbd57ecff24fd3
Callum Beckwith <123callumb@gmail.com>
2019-01-17 12:28:56 +0000

    Busienss Dash
    Update to has lead or not...

--- src/js/comp/Dashboard/BusinessDash.js
+++ src/js/comp/Dashboard/BusinessDash.js
@@ -12,6 +12,9 @@ import TextField from '@material-ui/core/TextField';
 import SearchIcon from '@material-ui/icons/Search';
 import FilterIcon from '@material-ui/icons/FilterList';
 import BusinessPlace from './BusinessPlace';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import VerifiedIcon from '@material-ui/icons/HowToReg';
+import NotVerIcon from '@material-ui/icons/AccountCircleRounded';
 
 
 
@@ -43,10 +46,25 @@ export default class BusinessDash extends Component {
             this.setState({viewBusiness: true});
         });
     }
-    fetchSearchData(){
-        (async() => {
-            const searchReq = await fetch('api/business.php');
-        })();
+    fetchSearchData(val){
+        if(val != '' || val != null || val != ' '){
+            (async() => {
+                const searchReq = await fetch('api/business.php?type=search&query=' + val, {
+                    method: 'GET'
+                });
+
+                const searchRes = await searchReq.json();
+
+                if(searchRes.data){
+                    this.setState({isSearching: true, tempSearchData: searchRes.data});
+                }else{
+                    this.setState({isSearching: false, tempSearchData: null});
+                }
+
+            })();
+        }else{
+            this.setState({isSearching: false, tempSearchData: null});
+        }
     }
     render(){
         return(
@@ -55,22 +73,32 @@ export default class BusinessDash extends Component {
                     <h2>Businesses</h2>
                     <Grid container>
                         <Grid item xs={2} md={2}><span><SearchIcon style={{marginTop: '5%'}}/></span></Grid>
-                        <Grid item xs={8} md={8}><TextField placeholder="Search Business Name/Type" style={{width: '100%'}}/></Grid>
+                        <Grid item xs={8} md={8}><TextField placeholder="Search Business Name/Type" style={{width: '100%'}} onChange={(e) => this.fetchSearchData(e.target.value)}/></Grid>
                         <Grid item xs={2} md={2}><span><FilterIcon style={{marginTop: '5%'}}/></span></Grid>
                     </Grid>
                 </div>
                 <List>
                     <ListItem style={{padding: '5px 0'}}>
+                        <ListItemIcon><b>Lead</b></ListItemIcon>
                         <ListItemText primary={<b>Name:</b>} />
                         <ListItemSecondaryAction><b>Dist. (Miles)</b></ListItemSecondaryAction>
                     </ListItem>
-                    {this.state.data ? 
-                        this.state.data.map((e, i) => {
-                            return <BusinessItem placeData={e} key={i} setBusiness={this.setBusiness.bind(this)}/> 
-                        })
-                        :
-                        <LinearProgress color="secondary" variant="indeterminate" />
+                    {this.state.isSearching == true ? 
+                        this.state.tempSearchData ? 
+                            this.state.tempSearchData.map( (e, i) => {
+                                return <BusinessItem placeData={e} key={i} setBusiness={this.setBusiness.bind(this)}/>
+                            })
+                            :
+                            <LinearProgress color="secondary" variant="indeterminate" />
+                    :
+                        this.state.data ? 
+                            this.state.data.map((e, i) => {
+                                return <BusinessItem placeData={e} key={i} setBusiness={this.setBusiness.bind(this)}/> 
+                            })
+                            :
+                            <LinearProgress color="secondary" variant="indeterminate" />
                     }
+                    
                 </List>
                 <BusinessPlace enabled={this.state.viewBusiness} disableHandler={this.closeBusiness.bind(this)} data={this.state.currentBusiness}/>
             </AbstractDash>
@@ -87,12 +115,13 @@ class BusinessItem extends Component {
         return(
             <Grow in={true}>
                 <ListItem button onClick={() => this.props.setBusiness(this.props.placeData)} style={{padding: '10px 0', margin: '10px 0px', boxShadow: '1px 2px 1px rgb(40, 40, 40, 0.3)', borderBottom: this.props.placeData.hasActiveLead ? 'solid #00a4c4 2px' : 'none'}}>
+                    <ListItemIcon style={{padding: '10px'}}>{this.props.placeData.hasActiveLead ? <span><VerifiedIcon /></span> : <span><NotVerIcon /></span>}</ListItemIcon>
                     <ListItemText 
                         style={{maxWidth: '75%', paddingLeft: '10px'}}
                         primary={this.props.placeData.name}
                         secondary={this.props.placeData.businessType}
                     />
-                    <ListItemSecondaryAction style={{paddingRight: '10px'}}>{this.props.placeData.distance.toFixed(2)}</ListItemSecondaryAction>
+                   {this.props.placeData.distance ? <ListItemSecondaryAction style={{paddingRight: '10px'}}>{this.props.placeData.distance.toFixed(2)}</ListItemSecondaryAction> : null }
                 </ListItem>
             </Grow>
         );
